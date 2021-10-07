885443732fe12602003f1a753482aef5eb42bbd1
ThangDao01 <72856471+ThangDao01@users.noreply.github.com>
2021-10-07 23:36:51 +0700

    Add project files.

---
+++ Student-List/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Student_List.Models;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Student_List
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private List<Student> ListStudent;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            ListStudent = StudentManage.GetStudents();
+        }
+
+        private void AddStudent_Click(object sender, RoutedEventArgs e)
+        {
+            var student = new Student();
+            student.Name = NameTextBox.Text;
+            student.BirthDay= BirthDayTextBox.Text;
+            student.ConverImage = AvatarTextBox.Text;
+            ListStudent.Add(student);
+            ListStudent.Add(new Student { Name = "Nguyen van C", BirthDay = "10/06/2021", ConverImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/220px-User_icon_2.svg.png" });
+        }
+    }
+}
---
+++ Student-List/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_List.Models
+{
+    public class Student
+    {
+        public string ConverImage { get; set; }
+        public string Name { get; set; }
+        public string BirthDay { get; set; }
+    }
+    public class StudentManage
+    {
+        public static List<Student> GetStudents()
+        {
+            var students = new List<Student>();
+            students.Add(new Student { Name = "Nguyen van a",BirthDay="10/06/2021",ConverImage= "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/220px-User_icon_2.svg.png"});
+            students.Add(new Student { Name = "Nguyen van B",BirthDay="10/06/2021",ConverImage= "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/220px-User_icon_2.svg.png"});
+            students.Add(new Student { Name = "Nguyen van C",BirthDay="10/06/2021",ConverImage= "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/220px-User_icon_2.svg.png"});
+            return students;
+        }
+    }
+
+
+}
